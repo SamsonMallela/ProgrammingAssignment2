cdafda4dda136a51bb21cbeeceeab48af56d7f86
smallela <samson.mallela@gmail.com>
2015-04-25 21:04:04 -0400

    makeCacheMatrix created
    created a function makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,29 @@
 ## Put comments here that give an overall description of what your
-## functions do -- Test Commit
 
-## Write a short comment describing this function
+
+## makeCacheMatrix is a function which creates a list of functions as below
+## "set" to set the matrix
+## "get" to get the matrix
+## "setinverse" to set the inverse of a matrix
+## "getinverse" top get the inverse of a matrix
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
+inv <- NULL
+set <- function (y){
+      x<-y
+      inv <<- NULL
+}
+get <- function () x
+setinverse <- function (inverse) inv <<- inverse
+getinverse >- function() inv
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function whihc checkis if a inverse matrix exists, 
+## if so it skips the computation and returns from the stored cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
